1b51368c51a51b5d0a03cbb92e129a760f33f221
Jerrod Bolton <85716225+JerrodBolton@users.noreply.github.com>
2025-02-25 03:43:07 -0700

    Changes to the JS
    I changed the javaS with change the stayle of the image.

---  JS/moveImage.js
+++  JS/moveImage.js
@@ -1,8 +1,8 @@
 // Jerrod Bolton 
 // 4/20/2021
 let img = document.getElementById('memeImage') 
-let x = Math.floor(Math.random() * 300);
-let y = Math.floor(Math.random() * 300);
+let x = Math.floor(Math.random() * 2000);
+let y = Math.floor(Math.random() * 2000);
 console.log(x);
 console.log(y);
 // this is a simple function that is going to move a image around the screen
@@ -12,14 +12,18 @@ console.log(y);
 
 // I think that I need to make a funcation that will change the position of the image
  move = () => {
-  
-    img.style.left = x + 'px';
-    img.style.top = y + 'px';
+    // I think that is random number make needs to be here so that I get new numbers for my top and left
+    // let x = Math.floor(Math.random() * 1000);
+    // let y = Math.floor(Math.random() * 1000);       
+
+    // img.style.top = x + 'px';
+    // img.style.left = y + 'px';
+
     console.log('move function is working');
  }
 
 moveImage = () => {
-   setInterval(move, 6000);
+   setInterval(move, 3000);
 
 
    console.log('moveImage function is working');
