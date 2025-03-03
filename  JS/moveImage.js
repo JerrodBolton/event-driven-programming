a70806333f4c95ff68cd60c6ae2f96bd0bd6264c
Jerrod Bolton <85716225+JerrodBolton@users.noreply.github.com>
2025-03-02 17:02:11 -0700

    Disable the button
    That mean that the button is disable

---  JS/moveImage.js
+++  JS/moveImage.js
@@ -1,41 +1,34 @@
 // Jerrod Bolton 
 // 4/20/2021
 let img = document.getElementById('memeImage');
-// let img  = document.createAttribute('img');
-// let x = Math.floor(Math.random() * 2000);
-// let y = Math.floor(Math.random() * 2000);
-// console.log(x);
-// console.log(y);
-// this is a simple function that is going to move a image around the screen
+let startButton = document.getElementById('moveImg');
+let stopButton = document.getElementById('stopImg');
 
-
-// let moveImage = document.getElementById('moveImage'); 
 
 // I think that I need to make a function that will change the position of the image
 let leftPos = 0;
 let myReq;
 
- move = (timeStamp) => {
-   // I think that is random number make needs to be here so that I get new numbers for my top and left. 
+ moveLeft = (timeStamp) => {
+
    leftPos += 0.4;
    img.style.left = leftPos + "px";
-   myReq = requestAnimationFrame(move);
-
+   myReq = requestAnimationFrame(moveLeft);
+   startButton.setAttribute('disabled', 'true');
+   stopButton.removeAttribute('disabled', 'false');
+   
  }
-
 stopImage = () => {
     cancelAnimationFrame(myReq);
     img.style.left = leftPos + "px";
     console.log('stopImage function is working');
-    
+    stopButton.setAttribute('disabled', 'true');
+    startButton.removeAttribute('disabled', 'false');
 }
-// cancelAnimationFrame(move);
-// moveImage = () => {
-// //    setInterval(move, 400);
-// //    console.log('moveImage function is working');
 
 
 
 
-// }
-// moveImage();
+
+
+
